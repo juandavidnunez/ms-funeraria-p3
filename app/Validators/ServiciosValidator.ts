666cf920e3fb7e9b17e3d3ed1531b89366246d6a
carlossoto12 <carlos.soto36945@ucaldas.edu.co>
2024-06-06 22:34:43 -0500

    relaciones init

--- app/Validators/ServiciosValidator.ts
+++ app/Validators/ServiciosValidator.ts
@@ -3,7 +3,8 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 export const servicioValidation = {
   schema: schema.create({
     descripcion: schema.string.optional({}, [
-      rules.maxLength(250)
+      rules.maxLength(250),
+      rules.required()
     ]),
     estado_servicio: schema.boolean([
       rules.required()
